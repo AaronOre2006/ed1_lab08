9ea472136a88f528bf6c453d05bbff78b564c7ca
Aaron Orellana <aaronmoises520@gmail.com>
2026-03-18 23:33:06 -0600

    Ejercicio no.01

--- src/main/java/ed/lab/E01KthSmallest.java
+++ src/main/java/ed/lab/E01KthSmallest.java
@@ -1,9 +1,26 @@
 package ed.lab;
+import java.util.Stack;
 
 public class E01KthSmallest {
 
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        int position = 0;
+        Stack<TreeNode<Integer>> st = new Stack<>();
+        TreeNode<Integer> current = root;
+
+        while (current != null || !st.isEmpty()){
+            while(current != null){
+                st.push(current);
+                current = current.left;
+            }
+            current = st.pop();
+            position += 1;
+            if(position == k){
+                return current.value;
+            }
+            current = current.right;
+        }
+        return -1;
     }
 
 }
